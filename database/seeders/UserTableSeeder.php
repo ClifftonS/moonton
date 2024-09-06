e6f80c0f41c50b704854983ba309f613c2e6c47a
ClifftonS <75228978+ClifftonS@users.noreply.github.com>
2024-09-06 14:24:25 +0700

    feat: set up spatie

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@moonton.test',
+            'password' => Hash::make('password')
+        ]);
+        $admin->assignRole('admin');
+    }
+}
